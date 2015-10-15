1d5e3a33ab2c4fb0036e25d9d31ee197ff542ddd
Salvador <Salvador@Salvador-PC>
2015-10-15 10:32:53 +0100

    Tercer commit 	- Nombres del fichero

--- src/practica3/is2/Main.java
+++ src/practica3/is2/Main.java
@@ -1,31 +1,24 @@
 package practica3.is2;
+import java.io.BufferedReader;
+import java.io.FileReader;
 import java.util.HashMap;
-
+import java.lang.Exception;
 public class Main {
 
     public static void main(String[] args) {
         Histogram<String> histo = new Histogram<>();
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        histo.increment("gmail.com");
-        
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        histo.increment("ulpgc.com");
-        
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        histo.increment("ull.com");
-        
+        try{
+           String name = "C:\\Users\\Salvador\\Documents\\0-Salvador\\ULPGC\\TERCERO\\Netbeans\\Practica3 IS2\\emailsfilev1.txt";
+           BufferedReader fileIn = new BufferedReader(new FileReader(name));
+           String mail;
+           
+           while((mail = fileIn.readLine())!= null){
+               if(mail.contains("@")){
+                   histo.increment(mail.split("@")[1]);
+               }
+           }
+        }catch(Exception e){
+        }
         HistogramDisplay histo1 = new HistogramDisplay(histo);
         histo1.execute();
     }
